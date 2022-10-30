3dd4eedea2c885bcdda4ba4c2bd66499e37d9e58
Jack Zhang <zhiyi.zhang.duke@gmail.com>
2022-10-29 22:26:37 -0400

    Initial commit with working resolvers

---
+++ database/database.go
@@ -0,0 +1,83 @@
+package database
+
+// https://www.section.io/engineering-education/implement-graphql-api-using-golang-and-mongodb-database/
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+
+	"example/graph/model"
+)
+
+type DB struct {
+	client *mongo.Client
+}
+
+func Connect(dbUrl string) *DB {
+	client, err := mongo.NewClient(options.Client().ApplyURI(dbUrl))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel = context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &DB{
+		client: client,
+	}
+}
+
+func (db *DB) FindCardTierScoreById(id string) *model.CardTier {
+	ObjectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cardTiersColl := db.client.Database("SlayTheSpire").Collection("CardsTierList")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	res := cardTiersColl.FindOne(ctx, bson.M{"_id": ObjectID})
+
+	cardTier := model.CardTier{ID: id}
+
+	res.Decode(&cardTier)
+
+	return &cardTier
+}
+
+func (db *DB) FindCardTierScoreByName(cardName string) *model.CardTier {
+	cardTiersColl := db.client.Database("SlayTheSpire").Collection("CardsTierList")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	result := cardTiersColl.FindOne(ctx, bson.M{"Card": cardName})
+
+	var cardTier model.CardTier
+	result.Decode(&cardTier)
+
+	return &cardTier
+}
+
+func (db *DB) FindCardTierScoreByClass(cardName string) []*model.CardTier {
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module example
+
+go 1.16
+
+require (
+	github.com/99designs/gqlgen v0.17.16
+	github.com/vektah/gqlparser/v2 v2.5.0
+	go.mongodb.org/mongo-driver v1.10.1
+)
